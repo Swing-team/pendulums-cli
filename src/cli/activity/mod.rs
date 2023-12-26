8142600c0c8890f0679f16029f390a14b6ec4fad
arminghm <armin.ghoreishi@gmail.com>
2023-12-26 18:14:35 +0330

    feat: add delete activity command
    TG-3

--- src/cli/activity/mod.rs
+++ src/cli/activity/mod.rs
@@ -1,9 +1,12 @@
 pub mod start_activity;
 pub mod get_current_activity;
 pub mod stop_current_activity;
+pub mod delete_activity;
 
 use clap::{Parser, Subcommand};
 use start_activity::StartActivityArgs;
+
+use self::delete_activity::DeleteActivityArgs;
 
 #[derive(Debug, Parser)]
 #[command(author = "Armin Ghoreishi", version, about, arg_required_else_help = true)]
@@ -23,5 +26,8 @@ pub enum ActivitySubCommands {
   GetCurrentActivity,
 
   #[command(name = "stop")]
-  StopCurrentActivity
+  StopCurrentActivity,
+
+  #[command(name = "delete")]
+  DeleteActivity(DeleteActivityArgs)
 }
